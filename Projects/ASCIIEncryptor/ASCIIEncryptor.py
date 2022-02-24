3bc896b639bdb870f684283e5139fc03022960d2
Yoinnkkk <wojpaw2007@gmail.com>
2022-02-24 09:12:42 +0000

    awd

--- Projects/ASCIIEncryptor/ASCIIEncryptor.py
+++ Projects/ASCIIEncryptor/ASCIIEncryptor.py
@@ -1,9 +1,19 @@
-
-#import array file
-art = open("asciiart.txt", "r")
-print(art)
+#imports
+import string
+#import array file along with other variables
+art = open("Projects/ASCIIEncryptor/ASCIIArt.txt", "r").read()
+alphabet = string.printable
 # main function
 
-def encrypt():
-    
-    return 
+def encrypt(object):
+    encryptedarray = []
+    counter = 0
+    for alphabet in object:
+        if len(alphabet) > 0:
+            encryptedarray.append(alphabet)
+            encryptedarray.pop(encryptedarray.index(alphabet))
+            counter+=1
+            print(counter)
+    return encryptedarray
+
+print(encrypt(art))
