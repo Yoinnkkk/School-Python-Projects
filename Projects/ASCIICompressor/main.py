e6b84debf1e0c098ceac84f00ce441a977fa939d
Yoinnkkk <wojpaw2007@gmail.com>
2022-03-25 10:44:22 +0000

    done

--- Projects/ASCIICompressor/main.py
+++ Projects/ASCIICompressor/main.py
@@ -1,5 +1,6 @@
 from ASCIICompressor import compress
 from ASCIIDecompressor import decompress
+import os
 import time as t
 def inputconfiged(string):
     return input(string + "\n:::   ")
@@ -8,9 +9,25 @@ def wait(time):
     t.sleep(time)
 
 def menu():
-    Question = inputconfiged("Choose an option from the ones below:\nCompress\nDecompress\nDisplay\nQuit")
-
-    if Question.lower() == "compress":
+    Question = inputconfiged("Choose an option from the ones below:\nEnter RLE\nCompress\nDecompress\nDisplay\nQuit")
+    if Question.lower() == "enter rle":
+        rleline = []
+        Question = int(inputconfiged("How many lines?"))
+        while Question < 2:
+            print("Please enter more than 2 lines!")
+            menu()
+        for i in range(Question):
+            rleline.append(inputconfiged("Please input line "+ str(i+1)))
+        rleline = ''.join(rleline)
+        opened = open("Projects/ASCIICompressor/Compressedtemp.txt", "w")
+        opened.write(rleline)
+        decompress("Compressedtemp.txt", "Decompressedtemp.txt")
+        os.remove("Projects/ASCIICompressor/Compressedtemp.txt")
+        file = open("Projects/ASCIICompressor/Decompressedtemp.txt", "r").read()
+        print(file)
+        os.remove("Projects/ASCIICompressor/Decompressedtemp.txt")
+        menu()
+    elif Question.lower() == "compress":
         Question = inputconfiged("What is the name of the object you would like to compress? ")
         Question2 = inputconfiged("What is the file you would like to output to? ")
         compress(Question, Question2)
