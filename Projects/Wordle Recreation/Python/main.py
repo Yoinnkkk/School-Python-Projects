d954f97b461ee897de873c0460ec38ed1223ae93
Yoinnkkk <wojpaw2007@gmail.com>
2022-04-17 14:00:15 +0000

    work

--- Projects/Wordle Recreation/Python/main.py
+++ Projects/Wordle Recreation/Python/main.py
@@ -1,3 +1,4 @@
+from word import tword
 def inputc(string):
     return input(string + "\n:::   ")
 
@@ -8,15 +9,27 @@ class Canvas:
         self.tries = tries
         self.triesused = 0
         self.usedwords = []
-        self.tiles = list(list())
+        self.tiles = []
+        self.correct = False
+        while len(self.tiles) != tries:
+            self.tiles.append([])
+            print(self.tiles)
+            
+#first array in tiles defines tries
+#second array inside tiles defines word
+def display():
+    x=0
+    while x < len(canvas.tiles):
+        print(canvas.tiles)
+        x+=1
 
-
-def start(l, t):
+def main(l, t):
     canvas = Canvas(l, t)
-    print(canvas.tries)
-    return 
-
+    while canvas.correct != True:
+        display()
+        
+    return
 
 while True:
-    options = [inputc("How long would you like the words to be? (default:5)") or 5, inputc("How many tries would you like to have? (default:6)") or 6]
-    start(options[0],options[1])
+    options = [inputc("How long would you like the words to be? (default:5)") or 5, inputc("How many tries at guessing would you like to have? (default:6)") or 6]
+    main(options[0],options[1])
